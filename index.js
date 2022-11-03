dc9921622168382d27ec2640ea4bb2abe9c19022
fares <faresqaisi998@gmail.com>
2022-11-02 23:21:19 -0700

    test 1 done

--- index.js
+++ index.js
@@ -46,9 +46,11 @@ Use the copy function below to do the following:
 */
 
 
-function copy(/*your code here*/){
-  /*your code here*/
+function copy(flavors){
+  const flavor = ["Banana Nut Fudge","Black Walnut","Burgundy Cherry","Butterscotch Ribbon","Cherry Macaron","Chocolate","Chocolate Almond","Chocolate Chip","Chocolate Fudge","Chocolate Mint","Chocolate Ribbon","Coffee","Coffee Candy","Date Nut","Eggnog","French Vanilla","Green Mint Stick","Lemon Crisp","Lemon Custard","Lemon Sherbet","Maple Nut","Orange Sherbet","Peach","Peppermint Fudge Ribbon","Peppermint Stick","Pineapple Sherbet","Raspberry Sherbet",  "Rocky Road","Strawberry","Vanilla","Vanilla Burnt Almond"];
+return flavor
 }
+copy()
 
 
 
